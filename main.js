f6b447994bdd3e87ed20a784bad6f6f2487dfd33
rileyhales <rileyhales1@gmail.com>
2025-05-04 19:27:19 -0600

    add park counters

--- main.js
+++ main.js
@@ -27,6 +27,33 @@ const placeToggleMenu = element => {
   document.body.appendChild(menu);
 };
 
+const placeParkMenu = element => {
+  const menu = document.createElement('div');
+  menu.classList.add('interact-menu');
+  const noElement = document.createElement('img');
+  noElement.onclick = () => decrementPark(element)
+  noElement.src = './no.png';
+  noElement.alt = 'Cancel';
+  noElement.classList.add('menu-icon');
+  const yesElement = document.createElement('img');
+  yesElement.onclick = () => incrementPark(element)
+  yesElement.src = './yes.png';
+  yesElement.alt = 'Apply';
+  yesElement.classList.add('menu-icon');
+  menu.appendChild(noElement);
+  menu.appendChild(yesElement);
+  // the no button should be to the left of the element and the yes button to the right
+  const rect = element.getBoundingClientRect();
+  menu.style.top = `${window.scrollY + rect.top - 8}px`;
+  menu.style.left = `${window.scrollX + rect.left - 50}px`;
+  menu.style.width = `${rect.width + 100}px`;
+  menu.style.height = `${rect.height}px`;
+  menu.style.display = 'flex';
+  menu.style.justifyContent = 'space-between';
+
+  document.body.appendChild(menu);
+}
+
 const addToggle = element => {
   if (!element.classList.contains('toggled')) {
     element.classList.add('toggled');
@@ -41,12 +68,51 @@ const removeToggle = element => {
   hideAllMenus()
 }
 
+const incrementPark = element => {
+  // list the children of the element in order
+  const children = Array.from(element.children);
+  hideAllMenus()
+  // the last element is a placeholder for spacing. if the 2nd to last is toggled, return
+  if (children[children.length - 2].classList.contains('toggled')) {
+    return false
+  }
+  children
+    .slice(0, -1)
+    .every(child => {
+      if (!child.classList.contains('toggled')) {
+        child.classList.add('toggled');
+        return false
+      }
+      return true
+    })
+}
+
+const decrementPark = element => {
+  // if the first element is not toggled then quit early
+  const children = Array.from(element.children);
+  hideAllMenus()
+  if (!children[0].classList.contains('toggled')) {
+    return false
+  }
+  // go in reverse order starting from the 2nd to last element and remove the toggle on the first toggled element
+  children
+    .slice(0, -1)
+    .reverse()
+    .every(child => {
+      if (child.classList.contains('toggled')) {
+        child.classList.remove('toggled');
+        return false
+      }
+      return true
+    })
+}
+
 const hideAllMenus = () => {
   document.querySelectorAll('.highlight-box').forEach(e => e.remove());
   document.querySelectorAll('.interact-menu').forEach(e => e.remove());
 }
 
-document.querySelectorAll('.fence, .house, .pool').forEach(element => {
+document.querySelectorAll('.fence, .house, .pool, .park').forEach(element => {
   element.addEventListener('click', () => {
     hideAllMenus()
     // Get the bounding box of the clicked element
@@ -69,10 +135,11 @@ document.querySelectorAll('.fence, .house, .pool').forEach(element => {
     const type = element.classList.contains('fence') ? 'fence'
       : element.classList.contains('house') ? 'house'
         : element.classList.contains('pool') ? 'pool'
-          : null;
+          : element.classList.contains('park') ? 'park'
+            : null;
     if (!type) return;
 
     if (type === "fence" || type === "pool") placeToggleMenu(element)
-
+    if (type === "park") placeParkMenu(element)
   });
 });
